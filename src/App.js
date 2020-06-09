0c874279d29ea9eee9dc142dc625014235469067
gabriel <vieyra.gabriel08@gmail.com>
2020-06-09 20:25:12 -0300

    add sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/style.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Spotify</h1>
+      <a href="#!">ssssssss</a>
+    </>
   );
 }
 
