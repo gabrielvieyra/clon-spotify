ac46c9c224c164eb3fbb603e91bc41aa7ed8cf40
gabriel <vieyra.gabriel08@gmail.com>
2020-06-19 17:44:15 -0300

    ajustando detalles main-area

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./styles/style.scss";
 import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
 import MainArea from "./components/MainArea";
 import Playback from "./components/Playback";
 
@@ -10,11 +9,7 @@ function App() {
     <div className="container">
       <div className="content">
         <Sidebar />
-        <div>
-          <Header />
-
-          <MainArea />
-        </div>
+        <MainArea />
       </div>
       <Playback />
     </div>
--- src/components/MainArea.js
+++ src/components/MainArea.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faChevronCircleLeft, faChevronCircleRight, faUser, faCaretDown} from '@fortawesome/free-solid-svg-icons';
 
 const MainArea = () => {
     return (
         <div className="main-area">
-            Main area
+           <header className="header">
+                <div className="header-flechas">
+                    <FontAwesomeIcon className="flecha" icon={faChevronCircleLeft} />
+                    <FontAwesomeIcon className="flecha" icon={faChevronCircleRight} />
+                </div>
+                <div className="desplegable">
+                    <button className="desplegable-user">
+                        <FontAwesomeIcon className="user-icon" icon={faUser} />
+                        <span className="user-name">gabriel.vieyra</span>
+                        <FontAwesomeIcon icon={faCaretDown} />
+                    </button>
+                </div>
+            </header>
+
+            <main>
+                <section className="section">
+                    <div className="section-header">
+                        <h2 className="section-titulo">Escuchados recientemente</h2>
+                    </div>
+
+                </section>
+            </main>
+            
         </div>
     )
 }
