2ce077d168f821000ffcafa0875b153891a3ae0e
gabriel <vieyra.gabriel08@gmail.com>
2020-06-19 16:04:54 -0300

    header terminado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faChevronCircleLeft, faChevronCircleRight, faUser, faCaretDown} from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     return (
         <div className="header">
-            Header part
+            <div className="header-flechas">
+                <FontAwesomeIcon className="flecha" icon={faChevronCircleLeft} />
+                <FontAwesomeIcon className="flecha" icon={faChevronCircleRight} />
+            </div>
+            <div className="desplegable">
+                <button className="desplegable-user">
+                    <FontAwesomeIcon className="user-icon" icon={faUser} />
+                    <span className="user-name">gabriel.vieyra</span>
+                    <FontAwesomeIcon icon={faCaretDown} />
+                </button>
+            </div>
         </div>
     )
 }
